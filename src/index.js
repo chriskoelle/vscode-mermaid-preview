7bae3426471876356fc524d46c2e9815eb8d3302
Chris Koelle <chris.koelle@hearst.com>
2021-08-07 01:06:44 -0400

    Add configuration for mermaid theme

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 "use strict"
+const {commands, workspace} = require('vscode');
 
-module.exports.activate = () => {
-    return {
-        extendMarkdownIt(md) {
-            const { highlight } = md.options;
-            md.options.highlight = (code, lang = '') =>
-              lang.match(/\bmermaid\b/i)
-                ? `<div class="mermaid">${code}</div>`
-                : highlight(code, lang);
+module.exports.activate = ({subscriptions}) => {
+  const mp = workspace.getConfiguration('mermaidPreview');
+  let theme = mp.get('theme');
 
-            return md;
-        }
+  /**
+   * Re-render Markdown if the Mermaid theme is changed
+   */
+  subscriptions.push(
+    workspace.onDidChangeConfiguration((e) => {
+      if (e.affectsConfiguration('mermaidPreview')) {
+        theme = workspace.getConfiguration('mermaidPreview').get('theme')
+        commands.executeCommand('markdown.preview.refresh');
+      }
+    })
+  )
+
+  /**
+   * Wrap code in Mermaid div and inject the theme
+   */
+  const mermaidIt = (code) =>
+  `<div class="mermaid">
+  %%{init:{'theme':'${theme}'}}%%
+  ${code}
+  </div>`;
+
+  return {
+    extendMarkdownIt(md) {
+      const {
+        highlight
+      } = md.options;
+      md.options.highlight = (code, lang = '') =>
+        lang.match(/\bmermaid\b/i)
+          ? mermaidIt(code)
+          : highlight(code, lang);
+      return md;
     }
-}
-
+  };
+};
